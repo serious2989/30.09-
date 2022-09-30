f9065b7dc811118f8777788f7d43482049e140b9
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-09-30 11:27:49 +0300

    Пример 1

---
+++ Пример 3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+for (int i = (-1) * n; i <= n; i+=2)
+    Console.Write(i + " ");
---
+++ пример 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: "); 
+int x1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите второе число: "); 
+int x2 = Convert.ToInt32(Console.ReadLine()); 
+if (x2 == x1 * x1)
+    Console.WriteLine("Второе число является квадратом первого"); 
+else
+    Console.WriteLine("Второе число не является квадратом первого");
